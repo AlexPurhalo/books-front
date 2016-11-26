b3eefdf955375be347375a1b034aabf2fc3d6b89
AlexPurhalo <alexpurhalo@gmail.com>
2016-11-26 10:24:08 +0200

    books-page-component-with-hardcoded-data

--- src/components/app.js
+++ src/components/app.js
@@ -6,12 +6,7 @@ export default class App extends Component {
 	render() {
 		return (
 			<div className="container">
-				<div className="parent-element-to-style">
-					Color: green
-					<div className="child-element-to-style">
-						Color: red
-					</div>
-				</div>
+				{this.props.children}
 			</div>
 		);
 	}
--- src/routes.js
+++ src/routes.js
@@ -4,8 +4,11 @@ import { Route, IndexRoute } from 'react-router';
 
 // Components import
 import App from './components/app';
+import BooksPage from './components/books/books-page';
 
 // Routes definition
 export default (
-	<Route path="/" component={App} />
+	<Route path="/" component={App}>
+		<IndexRoute component={BooksPage} />
+	</Route>
 );
